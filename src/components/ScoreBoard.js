d6079bd6e4645f4decb9218b0262da29d819c179
DannyHuacon <124841329+DannyHuacon@users.noreply.github.com>
2023-02-08 17:57:13 -0500

    Add files via upload

---
+++ src/components/ScoreBoard.js
@@ -0,0 +1,10 @@
+const ScoreBoard = ({ score }) => {
+  return (
+    <div className="score-board">
+       <h1>Reciclaje-JoseEscudero</h1>
+      <h2>{score}</h2>
+    </div>
+  )
+}
+
+export default ScoreBoard
